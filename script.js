16c3bf7f8c8282979c4f4c454c327cef62db43c3
Krissada Singhakachain <46844213+OmsinKrissada@users.noreply.github.com>
2022-10-30 16:58:16 +0700

    scaffold switch transition efffect

--- script.js
+++ script.js
@@ -1,15 +1,60 @@
+let games;
 fetch('https://gamertocoder.garena.co.th/api/minigames')
 	.then(res => res.json())
 	.then(data => {
-
-		const str = data.map(g => {
-
-			return `<li><img src="${g.icon}" width="100"></li>`;
-
-		});
-		console.log(str);
-
-		const ulELement = document.querySelector('#panel>ul');
-		ulELement.insertAdjacentHTML('beforeend', str.join(''));
-
+		games = data;
+		onFetched();
 	});
+
+
+async function onFetched() {
+
+	// construct panel list
+
+	const str = games.map(g => {
+		return `<li onclick="jumpTo(${g.no})"><img src="${g.icon}" width="100"></li>`;
+	});
+	// console.log(str);
+
+	const ulELement = document.querySelector('#panel>ul');
+	ulELement.insertAdjacentHTML('beforeend', str.join(''));
+
+	// construct title
+
+	const titleHolder = document.getElementById('title-holder');
+	for (const game of games) {
+		const element = document.createElement('h2');
+		element.innerText = game.name.toUpperCase();
+		element.classList.add('game', `game-${game.no}`, 'left');
+		titleHolder.appendChild(element);
+	}
+}
+
+let currentGameId = null;
+
+/**
+ * 
+ * @param {number} id game id 
+ */
+function setCoverDisplay(id) {
+	if (currentGameId) {
+		for (const element of document.getElementsByClassName(`game-${currentGameId}`)) {
+			element.classList.remove('active');
+		}
+	}
+	for (const element of document.getElementsByClassName(`game-${id}`)) {
+		element.classList.add('active');
+	}
+
+	currentGameId = id;
+}
+
+
+/**
+ * 
+ * @param {number} id game id
+ */
+function jumpTo(id) {
+	setCoverDisplay(id);
+	// alert(id);
+}
